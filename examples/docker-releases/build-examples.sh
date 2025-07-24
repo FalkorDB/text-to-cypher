8b10713b9940bf3bc03209505f8d76630085f15a
Barak Bar Orion <barak.bar@gmail.com>
2025-07-24 10:33:31 +0300

    Update Docker examples to use latest release with ARM64 support

--- examples/docker-releases/build-examples.sh
+++ examples/docker-releases/build-examples.sh
@@ -10,7 +10,7 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-VERSION=${1:-v0.1.0-alpha.3}
+VERSION=${1:-v0.1.0-alpha.12}
 PLATFORMS=${2:-"linux/amd64,linux/arm64"}
 
 echo -e "${YELLOW}Building Multi-Architecture Docker examples for text-to-cypher $VERSION${NC}"
